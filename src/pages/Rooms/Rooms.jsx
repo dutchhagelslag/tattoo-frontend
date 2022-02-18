532769b4698cc7be7c28ee8fd895d490bdcd165b
Min Lim <smlim991@macbook-pro.myfiosgateway.com>
2022-02-18 14:58:40 -0500

    Testing Get Call

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -17,80 +17,21 @@ export default function Rooms() {
 
   const history = useHistory();
 
-  useEffect(() => {
-    axios.get('https://demo-repo23.herokuapp.com/rooms/list')
-      .then((response) => {
+
+  const test_get = () => {
+    axios.get('https://ta2h3nna.herokuapp.com/all_designs')
+      .then(response => {
         console.log(response.data);
-        if (response.data){
-          setRooms(response.data);
-        }
-      })
+     })
       .catch(error => {
-        console.log(error);
-        setError(error);
+        console.log("API call failed");
       });
-  }, [refresh])
 
-  const handleCreateRoom = () => {
-    axios.post(`https://demo-repo23.herokuapp.com/rooms/create/${newRoomName}`)
-      .then(() => {
-        setIsModalOpen(false);
-        setRefresh(refresh + 1);
-      })
-      .catch(error => {
-        setError(error);
-        console.log(error);
-      })
-  }
-
+  };
   return (
     <div className="content">
-      {isModalOpen && 
-        <div className="create-modal">
-          <input
-            className="room-input"
-            placeholder="Room Name"
-            value={newRoomName}
-            onChange={(e) => setNewRoomName(e.target.value)}
-          />
-          <div className="create-actions">
-            <button className="button" onClick={handleCreateRoom}>Create New Room</button>
-            <button className="button" onClick={() => setIsModalOpen(false)}> Cancel </button>
-          </div>
-        </div>
-      }
-
-      <div className="rooms-header">
-        <h1>Rooms</h1>
-        <button
-          onClick={() => history.push('/')}
-          className="button"
-        >
-          {"<--"}Go Back Home
-        </button>
-      </div>
-
-      {error && (
-        <div className="rooms-error-box">
-          <p>{error.toString()}</p>
-        </div>
-      )}
-
-      <div className="rooms-list">
-        {rooms ? rooms.map((room, index) => (
-          <RoomItem
-            key={`${room.roomName}-${index}`}
-            name={room.roomName}
-            userCount={room.num_users}
-          />
-        )) : (
-          <div className="rooms-empty">
-            <p>Sorry there are no rooms right now... Come back later </p>
-          </div>
-        )}
-      </div>
-      <div>
-        <button className="page-button" onClick={() => setIsModalOpen(true)}> Add New Room </button>
+     <div>
+        <button className="page-button" onClick={() => test_get()}> Add New Room </button>
       </div>
     </div>
   )
