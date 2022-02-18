0bf6098053ed9610efacc175bf3a77fc552c4f4d
jimenacotera <jimenacotera@github.com>
2022-02-18 16:56:21 -0500

    added navbar to all pages with basic content and formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Navbar, Nav, Container} from 'react-bootstrap'; 
 
 import Home from './pages/Home/Home';
 import Rooms from './pages/Rooms/Rooms';
@@ -9,8 +10,23 @@ import './App.css';
 
 function App() {
   return (
+	<>
+	<div style={{ display: 'block', width: 700, padding: 30 }}>
+	        <h4>React-Bootstrap NavBar Component</h4>
+
+	<Navbar fixed="top" bg="light" variant="light">
+	      <Container>
+	      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+	      <Nav className="me-auto">
+	        <Nav.Link href="#home">Home</Nav.Link>
+	      </Nav>
+	      </Container>
+	 </Navbar>		
+	</div>
+
     <div className="root">
-      <div className="content">
+ 
+	  <div className="content">
         <Router>
           <Switch>
             <Route exact={true} path={'/'}>
@@ -26,6 +42,7 @@ function App() {
         </Router>
       </div>
     </div>
+	  </>
   );
 }
 
