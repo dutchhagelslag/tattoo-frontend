0e882a53b9e07e4fc858fd7440274eee4f861071
Min Lim <smlim991@macbook-pro.myfiosgateway.com>
2022-02-18 22:32:06 -0500

    fixed errors

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -30,20 +30,26 @@ export default function Gallery() {
                     <a
                         data-lg-size="1400-1400"
                         className="gallery-item"
-                        data-src="https://static.stereogum.com/uploads/2019/02/Lucki-1550606994.jpg"
-                        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@asoshiation' >Good</a></h4><p> 
+                        href="https://static.stereogum.com/uploads/2019/02/Lucki-1550606994.jpg"
+                        data-sub-html="<h4>Photo by - Good</h4><p> 
 					   Artist - Lucki</p>"
                     >
                         <img
+			    alt ="img"
                             className="img-responsive"
                             src="https://static.stereogum.com/uploads/2019/02/Lucki-1550606994.jpg"
                         />
                     </a>
 
-                <a data-lg-size="1406-1390" 
+                    <a 
+      		   data-lg-size="1406-1390" 
 	    	   href="https://external-preview.redd.it/datPPgSO8PbVrEIvDnB-oLvziOFin2ToDadRNdjbVbw.jpg?width=640&crop=smart&auto=webp&s=3959b5613a669040659e4fc28000d2d33c429c2a">
 
-                    <img alt="img2" src="https://external-preview.redd.it/datPPgSO8PbVrEIvDnB-oLvziOFin2ToDadRNdjbVbw.jpg?width=640&crop=smart&auto=webp&s=3959b5613a669040659e4fc28000d2d33c429c2a" />
+                    <img 
+			alt="img2" 
+			className="img-responsive"
+			src="https://external-preview.redd.it/datPPgSO8PbVrEIvDnB-oLvziOFin2ToDadRNdjbVbw.jpg?width=640&crop=smart&auto=webp&s=3959b5613a669040659e4fc28000d2d33c429c2a" />
+
                 </a>
 
 		<a href="https://preview.redd.it/msm091nhzci01.jpg?width=960&crop=smart&auto=webp&s=5dbcc05c38864f8fdc045a5f90f7bb127160e884">
