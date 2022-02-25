8993fe3f4246dd1e7f8bfbcdcc4afdc91661ae3a
Min Lim <smlim991@macbook-pro.myfiosgateway.com>
2022-02-25 18:57:53 -0500

    add to fonts page

--- src/pages/Fonts/Fonts.jsx
+++ src/pages/Fonts/Fonts.jsx
@@ -1,11 +1,41 @@
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
 
-export default function Rooms() {
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+import LightGallery from 'lightgallery/react';
 
-  return (
-    <div className="content">
-     <div>
-	  <h3>Here is where all the font previews will go</h3>
-     </div>
-    </div>
-  )
+import './fonts.css';
+
+export default function Gallery() {
+    const onInit = () => {
+        console.log('lightGallery has been initialized');
+    };
+
+    return (
+
+        <div className="App">
+            <LightGallery
+                onInit={onInit}
+	        mode="lg-fade"
+                plugins={[lgThumbnail, lgZoom]}
+            >
+                    <a
+                        data-lg-size="1400-1400"
+                        className="gallery-item"
+                        href="https://www.designworkplan.com/media/pages/read/arial-is-everywhere/0c09943d44-1617288500/arial_header_930x530-01.png"
+                        data-sub-html="<h4>Font Name: Arial</h4><p> 
+					   </p>"
+                    >
+                        <img
+			    alt ="img"
+                            className="img-responsive"
+                            src="https://www.designworkplan.com/media/pages/read/arial-is-everywhere/0c09943d44-1617288500/arial_header_930x530-01.png"
+                        />
+                    </a>
+
+            </LightGallery>
+        </div>
+    );
 }
