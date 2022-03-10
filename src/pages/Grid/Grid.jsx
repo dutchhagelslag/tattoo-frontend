e5a02a4f766699ebc9f77a952dda0a429376d49f
jimenacotera <jimenacotera@github.com>
2022-03-10 12:05:19 -0500

    added images for debugging, changed formatting of grid

--- src/pages/Grid/Grid.jsx
+++ src/pages/Grid/Grid.jsx
@@ -10,14 +10,31 @@ function App() {
 	thumbnailWidth: 600,
 	thumbnailHeight: 300,    
 	caption: "Artist: Jacob"
-    }
+    },
+    {
+	src: "https://i.pinimg.com/originals/8b/5a/8f/8b5a8fd8301c37f53f7ceb3990427111.jpg",
+	thumbnail:"https://i.pinimg.com/originals/8b/5a/8f/8b5a8fd8301c37f53f7ceb3990427111.jpg",
+	caption: "Artist: Ginkgo"
+    },
+    {
+	src: "https://i.pinimg.com/750x/1a/b4/7f/1ab47ff65232f8a327c43543e9d9c79c.jpg",
+	thumbnail: "https://i.pinimg.com/750x/1a/b4/7f/1ab47ff65232f8a327c43543e9d9c79c.jpg",
+	caption: "Hand lines",
+	thumbnailHeight: 300
+    },
+	 {
+		 src: "https://i.pinimg.com/750x/d6/6e/1c/d66e1c00e0889f8de2f783eaf94d9a35.jpg",
+		 thumbnail: "https://i.pinimg.com/750x/d6/6e/1c/d66e1c00e0889f8de2f783eaf94d9a35.jpg",
+		 caption: "@adrianmactattoo"
+    } 
+
   ];
 
   return(
-	<div style={{display: "block", width: "90%"}} > 	
+	<div style={{display: "block", width: "100%"}} > 	
 	    <h1>Tattoo Designs</h1> 
 	    <div><Gallery images={data}
-	        rowHeight= "180"
+	        rowHeight={300}
 	  	enableImageSelection={false}
 	    /> </div>
 	</div>
