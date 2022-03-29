d831fe1960bed8a5b3dd68275d4551cb5b72f7d1
pl1824 <patricktonylee@gmail.com>
2022-03-29 19:12:59 -0400

    odd github feature causeing errors

--- src/pages/Grid/Grid.jsx
+++ src/pages/Grid/Grid.jsx
@@ -13,9 +13,7 @@ function App() {
 	    Accept: "application/json",
 	    "Content-Type": "application/json;charset=UTF-8",
 	}
-    };
-
-<<<<<<< HEAD
+  };
 
     const to_data = image => ({src: image.src, 
 			       thumbnail: image.src, 
@@ -36,44 +34,6 @@ function App() {
     	        console.log(err);
     	    })
     });
-
-=======
-<<<<<<< HEAD
-    fetch(url, options)
-	.then(response => response.json())
-        .then(rsp_json => JSON.parse(rsp_json))
-	.then(images => {
-	    const to_data = image => ({src: image.src, 
-				       thumbnail: image.src, 
-				       thumbnailWidth: image.thumbnailWidth, 
-				       thumbnailHeight: image.thumbnailHeight, 
-				       caption: image.caption});
-	    const test = images.map(to_data);
-	    setData(test);
-	    console.log(test);
-	})
-	.catch (err =>{
-	    console.log(err);
-	})
-
-=======
-    useEffect(() => {
-        fetch(url, options)
-    	    .then(response => response.json())
-	    .then(response => JSON.parse(response))
-    	    .then(images => {
-    	        const to_data = image => ({src: image.src, thumbnail: image.src, thumbnailWidth: image.thumbnailWidth, thumbnailHeight: image.thumbnailHeight, caption: image.caption});
-    
-    	        const test = images.map(to_data);
-    	        setData(test);
-    	        console.log(test);
-    	    })
-    	    .catch (err =>{
-    	        console.log(err);
-    	    })
-    });
->>>>>>> bd07d594d9e382cc20566f420937e1ea748c3182
->>>>>>> baf74c6a9ddbb28d793874a1d3e9374d38615255
 
   return(
 	<div className="grid" > 	   
