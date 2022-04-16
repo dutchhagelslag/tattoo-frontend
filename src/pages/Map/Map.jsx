de14cdb04abe6c1711c5dcf93ac1ca3227e9edae
Min Lim <smlim991@macbook-pro.myfiosgateway.com>
2022-04-15 23:33:44 -0400

    Changed Map Style

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -7,8 +7,25 @@ import {
 } from "@react-google-maps/api";
 
 import "@reach/combobox/styles.css";
+import mapStyles from "./mapStyles";
+
 
 const libraries = ["places"];
+const mapContainerStyle = {
+    width: "80vw",
+    height: "80vh",
+};
+const center = {
+    lat: 40.69452,
+    lng: -73.98650,  
+};
+
+const options= {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true,
+};
+
 
 export default function App() {
     const {isLoaded, loadError} = useLoadScript({
@@ -18,5 +35,20 @@ export default function App() {
 
     if (loadError) return "Error loading maps";
     if (!isLoaded) return "Loading Maps";
-    return <div>map</div>;
+    return ( 
+        <div>
+            <h2>
+                Parlors{" "}
+                <span role= "img" aria-label = "Cool">
+                    👌
+                </span>
+            </h2>
+            <GoogleMap 
+                mapContainerStyle = {mapContainerStyle} 
+                zoom = {15} 
+                center = {center}
+                options={options}
+            ></GoogleMap>
+        </div>
+    );
 }
