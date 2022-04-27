17ac3b56ec3a3499463525ca7ce678b036d62b78
pl1824 <patricktonylee@gmail.com>
2022-04-27 02:46:21 -0400

    Added to file upload to formik

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -4,7 +4,6 @@ async function auth_upload_api_calls(){
     // calls the required apis for authorization
     // synchronously as we need info 
 
-
     // 1st api call: for token and entry url
     const api_token_url = "https://ta2h3nna.herokuapp.com/authorize_backblaze_access";
 
@@ -22,34 +21,36 @@ async function auth_upload_api_calls(){
     	  .then(response => {
               const url = Object.values(response)[3];
               const tok = Object.values(response)[4];
+              console.log([url, tok]);
               return [url, tok];
           });
 
     
-    // // 2nd api call: for backblaze upload url 
-    // const api_upload_url = token_entry[0];
-    // const api_token = token_entry[1];
-    // // eslint-disable-next-line
-    // const bucket_id = "8d5894f45da9ef2674e90913"; // hardcoded to ta2h3nna bucket will change
-
-    // const api_upload_options = {
-    // 	method: "GET",
-    // 	headers: {
-    // 	    Accept: "application/json",
-    //         "Authorization": api_token,
-    // 	    "Content-Type": "application/json;charset=UTF-8",
-    // 	}
-    // };
-
-    // const upload_url = await fetch(api_upload_url, api_upload_options)
-    // 	  .then(response => response.json())
-    // 	  .then(response => {
-    //           const tok = Object.values(response)[4];
-    //           return [tok];
-    //       });
-    
+    // 2nd api call: for backblaze upload url 
+    const api_upload_url = token_entry[0];
+    const api_token = token_entry[1];
 
 
+    // hardcoded to ta2h3nna bucket will change
+    // const bucket_id = "8d5894f45da9ef2674e90913"; 
+
+    const api_upload_options = {
+    	method: "GET",
+    	headers: {
+    	    Accept: "application/json",
+            'Authorization': api_token,
+    	    'Content-Type': "application/json;charset=UTF-8",
+            
+    	}
+    };
+
+    // eslint-disable-next-line
+    const upload_url = await fetch(api_upload_url, api_upload_options)
+    	  .then(response => response.json())
+    	  .then(response => {
+              console.log(response);
+          });
+ 
 
     // 3rd api call: upload image
     // const upload_img_url = "https://ta2h3nna.herokuapp.com/authorize_backblaze_access";
@@ -92,6 +93,8 @@ async function auth_upload_api_calls(){
 }
 
 
+// add validation schema for signup form
+
 const SignupForm = () => {
   return (
     <>
@@ -110,23 +113,25 @@ const SignupForm = () => {
         }}
 
         onSubmit={(values, { setSubmitting }) => {
-            
+            // do api call here with values
         }}
       >
         <Form>
           <Field type="text" name="tattoo name" placeholder="tattoo name" />
-
+          <br></br>
           <Field type="text" name="artist name" placeholder="artist name" />
-
+          <br></br>
           <Field type="text" name="genre" placeholder="genre" />
-
+          <br></br>
           <Field type="text" name="cost" placeholder="cost" />
-
+          <br></br>
           <Field type="text" name="caption" placeholder="caption" />
+          <br></br>
+          <br></br>
 
-          {/* <MyCheckbox name="acceptedTerms"> */}
-          {/*   I accept the terms and conditions */}
-          {/* </MyCheckbox> */}
+          <input id="file" name="file" type="file" onChange={(event) => {
+              Formik.setFieldValue("file", event.currentTarget.files[0]);
+          }} />
 
           <button type="submit">Submit</button>
         </Form>
