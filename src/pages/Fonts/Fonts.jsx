6bdb358aa37106093146c8f317b06d0579456db3
jimenacotera <jimenacotera@github.com>
2022-04-29 19:56:30 -0400

    moved input and reformatted flex

--- src/pages/Fonts/Fonts.jsx
+++ src/pages/Fonts/Fonts.jsx
@@ -22,7 +22,7 @@ class App extends Component {
 render() { 
  return(
 	 
-    <div class="box">
+    <div className="box">
         <h1 className="pageTitle">Fonts gallery</h1>
         <div className="realtimeInput" >
           <input 
